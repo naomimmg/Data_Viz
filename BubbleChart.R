001cb9590c8898b8f31656d257a3c42dd9edb5a8
naomimmg <72946482+naomimmg@users.noreply.github.com>
2022-04-05 17:24:10 -0400

    Add files via upload

--- BubbleChart.R
+++ BubbleChart.R
@@ -1,16 +1,17 @@
 
+##dataset downloaded from The World Bank (world development indicators dataset, original source: United Nations Population Division)
+##https://databank.worldbank.org/source/world-development-indicators
+#x-axis: The number of children that would be born to a woman if she were to live to the end of her childbearing years and bear children in accordance with age-specific fertility rates of the specified year
+#y-axis: The number of years a newborn infant would live if prevailing patterns of mortality at the time of its birth were to stay the same throughout its life.
 
 devtools::install_github("jcheng5/googleCharts")
 library(googleCharts)
 library(shiny)
-library(dplyr)
 
-dat <- readRDS("healthexp.Rds")
+data <- read.csv("newdat.csv")
 data$Region <- as.factor(data$Region)
 data<-na.omit(data)
 
-# Use global max/min for axes so the view window stays
-# constant as the user moves between years
 xlim <- list(
   min = min(data$Fertility.Rate) - 1,
   max = max(data$Fertility.Rate) + 1
@@ -21,10 +22,8 @@ ylim <- list(
 )
 
 ui <- fluidPage(
-  # This line loads the Google Charts JS library
-  googleChartsInit(),
-  
-  # Use the Google webfont "Source Sans Pro"
+  googleChartsInit(), #initializing
+  #change Google font
   tags$link(
     href=paste0("http://fonts.googleapis.com/css?",
                 "family=Source+Sans+Pro:300,600,300italic"),
@@ -32,17 +31,12 @@ ui <- fluidPage(
   tags$style(type="text/css",
              "body {font-family: 'Source Sans Pro'}"
   ),
-  
+  #for more stylistic options see https://developers.google.com/chart/interactive/docs/gallery/bubblechart
   googleBubbleChart("chart",
                     width="100%", height = "475px",
-                    # Set the default options for this chart; they can be
-                    # overridden in server.R on a per-update basis. See
-                    # https://developers.google.com/chart/interactive/docs/gallery/bubblechart
-                    # for option documentation.
                     options = list(
                       fontName = "Source Sans Pro",
                       fontSize = 13,
-                      # Set axis labels and ranges
                       hAxis = list(
                         title = "Fertility rate, total (births per woman)",
                         viewWindow = xlim
@@ -51,22 +45,17 @@ ui <- fluidPage(
                         title = "Life expectancy (years)",
                         viewWindow = ylim
                       ),
-                      # The default padding is a little too spaced out
                       chartArea = list(
                         top = 50, left = 75,
                         height = "75%", width = "75%"
                       ),
-                      # Allow pan/zoom
                       explorer = list(),
-                      # Set bubble visual props
                       bubble = list(
                         opacity = 0.4, stroke = "none",
-                        # Hide bubble label
                         textStyle = list(
                           color = "none"
                         )
                       ),
-                      # Set fonts
                       titleTextStyle = list(
                         fontSize = 16
                       ),
@@ -88,22 +77,13 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-  
-  # Provide explicit colors for regions, so they don't get recoded when the
-  # different series happen to be ordered differently from year to year.
-  # http://andrewgelman.com/2014/09/11/mysterious-shiny-things/
-  defaultColors <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  defaultColors <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7") #colours for regions
   series <- structure(
     lapply(defaultColors, function(color) { list(color=color) }),
     names = levels(data$Region)[2:8]
   )
   
   yearData <- reactive({
-    # Filter to the desired year, and put the columns
-    # in the order that Google's Bubble Chart expects
-    # them (name, x, y, color, size). Also sort by region
-    # so that Google Charts orders and colors the regions
-    # consistently.
     df <- data %>%
       filter(Year == input$year) %>%
       select(Country, Fertility.Rate, Life.Expectancy,
@@ -112,7 +92,6 @@ server <- function(input, output, session) {
   })
   
   output$chart <- reactive({
-    # Return the data and options
     list(
       data = googleDataTable(yearData()),
       options = list(
