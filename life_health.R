cf0fa4aba24350f79a07c5a539a42d0e74af247c
naomimmg <72946482+naomimmg@users.noreply.github.com>
2022-04-05 17:25:43 -0400

    Add files via upload

--- life_health.R
+++ life_health.R
@@ -1,130 +1,186 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
-
-devtools::install_github("jcheng5/googleCharts")
-library(googleCharts)
 library(shiny)
-library(dplyr)
+library(ggplot2)
+library(SimDesign)
+library(shinyjs)
 
-dat <- readRDS("healthexp.Rds")
-data$Region <- as.factor(data$Region)
-data<-na.omit(data)
+# Define UI for application that draws a histogram
+ui <- fluidPage(useShinyjs(),
+                fluidRow(
+                    column(width=8, align="center",
+                           tabsetPanel(id="tabs",
+                                       tabPanel(title = '', value = "intro",
+                                                fluidRow(
 
-# Use global max/min for axes so the view window stays
-# constant as the user moves between years
-xlim <- list(
-  min = min(data$Fertility.Rate) - 1,
-  max = max(data$Fertility.Rate) + 1
-)
-ylim <- list(
-  min = min(data$Life.Expectancy),
-  max = max(data$Life.Expectancy) + 3
-)
-
-ui <- fluidPage(
-  # This line loads the Google Charts JS library
-  googleChartsInit(),
-  
-  # Use the Google webfont "Source Sans Pro"
-  tags$link(
-    href=paste0("http://fonts.googleapis.com/css?",
-                "family=Source+Sans+Pro:300,600,300italic"),
-    rel="stylesheet", type="text/css"),
-  tags$style(type="text/css",
-             "body {font-family: 'Source Sans Pro'}"
-  ),
-  
-  googleBubbleChart("chart",
-                    width="100%", height = "475px",
-                    # Set the default options for this chart; they can be
-                    # overridden in server.R on a per-update basis. See
-                    # https://developers.google.com/chart/interactive/docs/gallery/bubblechart
-                    # for option documentation.
-                    options = list(
-                      fontName = "Source Sans Pro",
-                      fontSize = 13,
-                      # Set axis labels and ranges
-                      hAxis = list(
-                        title = "Fertility rate, total (births per woman)",
-                        viewWindow = xlim
-                      ),
-                      vAxis = list(
-                        title = "Life expectancy (years)",
-                        viewWindow = ylim
-                      ),
-                      # The default padding is a little too spaced out
-                      chartArea = list(
-                        top = 50, left = 75,
-                        height = "75%", width = "75%"
-                      ),
-                      # Allow pan/zoom
-                      explorer = list(),
-                      # Set bubble visual props
-                      bubble = list(
-                        opacity = 0.4, stroke = "none",
-                        # Hide bubble label
-                        textStyle = list(
-                          color = "none"
-                        )
-                      ),
-                      # Set fonts
-                      titleTextStyle = list(
-                        fontSize = 16
-                      ),
-                      tooltip = list(
-                        textStyle = list(
-                          fontSize = 12
-                        )
-                      )
-                    )
-  ),
-  fluidRow(
-    shiny::column(4, offset = 4,
-                  sliderInput("year", "Year",
-                              min = min(data$Year), max = max(data$Year),
-                              value = min(data$Year), animate = TRUE)
-    )
-  )
-)
+    # Sidebar with a slider input for number of bins 
+   
 
 
-server <- function(input, output, session) {
-  
-  # Provide explicit colors for regions, so they don't get recoded when the
-  # different series happen to be ordered differently from year to year.
-  # http://andrewgelman.com/2014/09/11/mysterious-shiny-things/
-  defaultColors <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-  series <- structure(
-    lapply(defaultColors, function(color) { list(color=color) }),
-    names = levels(data$Region)[2:8]
-  )
-  
-  yearData <- reactive({
-    # Filter to the desired year, and put the columns
-    # in the order that Google's Bubble Chart expects
-    # them (name, x, y, color, size). Also sort by region
-    # so that Google Charts orders and colors the regions
-    # consistently.
-    df <- data %>%
-      filter(Year == input$year) %>%
-      select(Country, Fertility.Rate, Life.Expectancy,
-             Region, Population) %>%
-      arrange(Region)
-  })
-  
-  output$chart <- reactive({
-    # Return the data and options
-    list(
-      data = googleDataTable(yearData()),
-      options = list(
-        title = sprintf(
-          "Fertility rate vs. life expectancy, %s",
-          input$year),
-        series = series
-      )
-    )
-  })
+        # Show a plot of the generated distribution
+            br(),
+            br(),
+            br(),
+            br(),
+            p(id = "greetings", "Welcome!! To start the study, press 'Next'"),
+            plotOutput("plot0"),
+            plotOutput("plot1"),
+            plotOutput("plot2"),
+            plotOutput("plot3"),
+            plotOutput("plot4"),
+        
+
+        sliderInput("cor0", label = p("Estimate the correlation", em("r")),
+                    min = -1, max = 1,
+                    value = 0, step = 0.05),
+        sliderInput("ask", label = p("What is the smallest meaningful association?"),
+                    min = -1, max = 1,
+                    value = 0, step = 0.05),
+        numericInput("perc", "What percent is the TARGET region of the total?", 0),
+        sliderInput("tar", label = p("At what percent does the TARGET region become predominant?"),
+                    min = 0, max = 100,
+                    value = 0, step = 1),
+        sliderInput("pie", label = p("At what percent does the TARGET region become predominant?"),
+                    min = 0, max = 100,
+                    value = 0, step = 1),
+        tags$script(HTML("
+        $(document).ready(function() {setTimeout(function() {
+          supElement = document.getElementById('tar').parentElement;
+          $(supElement).find('span.irs-single, span.irs-from, span.irs-to').remove();
+        }, 100);})
+      ")),
+        actionButton("button", "Next")
+        
+    
+))))))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    counter <- reactiveValues(countervalue=0)
+    observeEvent(input$button, {
+        counter$countervalue <- counter$countervalue + 1
+    })
+
+    df<-reactive( {
+        list_cor0 = c(-1, -0.95, -0.9, -0.85, -0.8, -0.75, -0.7, -0.65, -0.6, -0.55, -0.5, -0.45, -0.4, -0.35, -0.3, -0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1)
+        r0 = sample(list_cor0, size = 1)
+        
+        
+        xy0 <- data.frame(SimDesign::rmvnorm( #sample from a multivariate normal
+            n=500, #how many points
+            mean=c(0,0), #population mean
+            sigma=matrix(c(1,r0,r0,1),2,2) #population covariance
+        ))
+        
+        corr0<-cor(xy0$X1, xy0$X2)
+        
+        list(r0 = r0)
+        
+    })
+    
+    output$plot0 <- renderPlot({
+        
+        ggplot(data=df()$xy0,aes(xy0$X1,xy0$X2)) + geom_point(color="blue") + labs(x="X", y="Y") + theme_classic()
+        
+    })
+    
+    output$plot1 <- renderPlot({
+        
+        xy <- data.frame(rmvnorm( #sample from a multivariate normal
+            n=500, #how many points
+            mean=c(0,0), #population mean
+            sigma=matrix(c(1,input$ask,input$ask,1),2,2) #population covariance
+        ))
+        
+        ggplot(data=xy,aes(X1,X2)) + geom_point(color="blue") + labs(x="X", y="Y") + theme_classic()
+        
+    })
+    
+    output$plot2 <- renderPlot({
+        
+        dviz <- data.frame(
+            group = c("TARGET", "proportion 1", "proportion 2"),
+            value = c(25, 25, 50)
+        )
+        ggplot(dviz, aes(x="", y=value, fill=group))+
+            geom_bar(width = 0.25, stat = "identity")
+        
+    })
+    
+    output$plot3 <- renderPlot({
+        
+        dviz <- data.frame(
+            group = c("TARGET", "proportion 1", "proportion 2"),
+            value = c(input$tar, 25, 50)
+        )
+        ggplot(dviz, aes(x="", y=value, fill=group))+
+            geom_bar(width = 0.25, stat = "identity") + theme_void()
+        
+    })
+    
+    output$plot4 <- renderPlot({
+        
+        dviz <- data.frame(
+            group = c("TARGET", "proportion 1", "proportion 2"),
+            value = c(input$pie, 25, 50)
+        )
+        ggplot(dviz, aes(x="", y=value, fill=group))+
+            geom_bar(width = 0.25, stat = "identity") +coord_polar("y", start=0)+theme_void()
+        
+    })
+    
+    observe({
+        shinyjs::hide("plot0")
+        shinyjs::hide("cor0")
+        shinyjs::hide("plot1")
+        shinyjs::hide("ask")
+        shinyjs::hide("plot2")
+        shinyjs::hide("perc")
+        shinyjs::hide("plot3")
+        shinyjs::hide("tar")
+        shinyjs::hide("pie")
+        shinyjs::hide("plot4")
+        
+        if(counter$countervalue==1) {
+            shinyjs::show("plot0")
+            shinyjs::show("cor0")
+            shinyjs::hide("greetings")
+        }
+        
+        if(counter$countervalue==2) {
+            shinyjs::show("plot1")
+            shinyjs::show("ask")
+            shinyjs::hide("greetings")
+        }
+        
+        if(counter$countervalue==3) {
+            shinyjs::show("plot2")
+            shinyjs::show("perc")
+            shinyjs::hide("greetings")
+        }
+        
+        if(counter$countervalue==4) {
+            shinyjs::show("plot3")
+            shinyjs::show("tar")
+            shinyjs::hide("greetings")
+        }
+        
+        if(counter$countervalue==5) {
+            shinyjs::show("plot4")
+            shinyjs::show("pie")
+            shinyjs::hide("greetings")
+        }
+    })
+    
 }
 
+# Run the application 
 shinyApp(ui = ui, server = server)
-
-         
